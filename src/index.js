9debbad1fa92e789929a22051030a99837aeaad1
github-AKZhuk <lesha.zhuk@gmail.com>
2021-02-20 10:23:55 +0300

    refactor: apply prettier

--- src/index.js
+++ src/index.js
@@ -1,6 +1,20 @@
+module.exports = function towelSort(matrix) {
+    let ArrayInLine = [];
+    switch (matrix) {
+        case undefined:
+            break;
 
-// You should implement your task here.
+        default:
+            for (let i = 0; i < matrix.length; i++) {
+                if (i % 2 != 0) {
+                    matrix[i].reverse();
+                }
+                for (let n = 0; n < matrix[i].length; n++) {
+                    ArrayInLine.push(matrix[i][n]);
+                }
+            }
+            break;
+    }
 
-module.exports = function towelSort (matrix) {
-  return [];
-}
+    return ArrayInLine;
+};
